fbe17a8c06b7e88d63472844ae4e1f9ae6feacfd
xholes <njlmcu@163.com>
2016-03-08 22:34:41 +0800

    timelimit

--- HDOJ1007.cpp
+++ HDOJ1007.cpp
@@ -2,45 +2,58 @@
 #include<cmath>
 #include<iomanip>
 using  namespace std;
-double min(double *r,int);
+const int SIZE= 100005;
+typedef struct
+{
+	double x;
+	double y;
+}coord;
+
+coord dot[SIZE];
+double dis[SIZE];
+
+double min(int n)
+{
+	int index = 0;
+	for (int i = 0; i < n; i++)
+		if (dis[index]>dis[i])
+			index = i;
+	return dis[index];
+}
+
+double distance(int n)
+{
+	double temp, distance = 65536;
+	for (int num = 0; num < n; num++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (i == num) continue;
+			temp = sqrt(pow(dot[num].x - dot[i].x,2.0) + pow(dot[num].y - dot[i].y, 2.0));
+			if (temp < distance)
+				distance = temp;
+			
+		}
+	} 
+	return distance;
+}
+
+
+
+
 int main()
 {
-	int N,cnt=0;
-	double *x, *y, *r,R[100001],rmax,temp;
-	
-	while (cin >> N)
+	int n,cnt=0;
+	while (cin >> n && n!=0)
 	{
-		int k=0;
-		if (N == 0) break;
-		x = new double[N];
-		y = new double[N];
-		r = new double[N*(N-1)/2]();
-		for (int i = 0; i < N; i++)
-			cin >> x[i] >> y[i];
-		for (int i = 0; i < N; i++)
-			for (int j = i+1 ; j < N; j++,k++)
-			{ 
-				int cnt = 0;
-				temp = sqrt(pow((x[i] - x[j]),2)+pow((y[i] - y[j]),2))/2;		
-				r[k] = temp;		 
-			}				
-		rmax = min(r,k);
-		R[cnt] = rmax;
+		for (int i = 0; i < n;i++)
+			cin >> dot[i].x >> dot[i].y;
+		dis[cnt]=distance(n);
 		cnt++;
-		delete[]x;
-		delete[]y;
-		delete[]r;
 	}
-	for (int i = 0; i < cnt;i++)
-			cout <<fixed<<setprecision(2)<< R[i] << endl;
+	for (int i = 0; i < cnt; i++)
+		cout <<fixed<<setprecision(2)<< dis[i]/2 << endl;
 //	system("pause");
 	return 0;
 }
-double min(double *r,int cnt )
-{
-	int index= 0;
-	for (int i = 0; i < cnt; i++)
-		if (r[index] > r[i])
-			index = i;
-	return r[index];
-}
+ 
